49370da7abe904bf624b60682f6fbd438f0b3194
Alexey <alexey.novgorodov@brightsec.com>
2026-02-18 19:13:16 +0100

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/delete-users-v1-name1.test.ts
@@ -0,0 +1,45 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('DELETE /users/v1/name1', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['broken_access_control', 'jwt', 'id_enumeration', 'csrf'],
+      attackParamLocations: [AttackParamLocation.PATH, AttackParamLocation.HEADER],
+      starMetadata: {
+        code_source: 'abright25/star-VAmPI:original_main',
+        databases: ['SQLAlchemy'],
+        user_roles: ['admin']
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined,
+      testMetadata: {
+        broken_access_control: {
+          authObjectId: [null, 'test-auth-object-123']
+        }
+      }
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.DELETE,
+      url: `${baseUrl}/users/v1/name1`,
+      headers: { Authorization: 'Bearer <JWT_TOKEN>' }
+    });
+});
