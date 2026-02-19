331bd86318933784bbe26598d016e21ba037603a
Alexey <alexey.novgorodov@brightsec.com>
2026-02-19 15:05:12 +0100

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/put-users-v1-name1-email.test.ts
@@ -0,0 +1,57 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('PUT /users/v1/name1/email', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: [
+        {
+          name: 'broken_access_control',
+          options: {
+            auth: 'azpLwB6e5XU2tcsh5Rwnyh'
+          }
+        },
+        'email_injection',
+        'regex_dos',
+        'jwt',
+        'sqli'
+      ],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'abright25/star-VAmPI:master',
+        databases: ['SQLAlchemy'],
+        user_roles: []
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.PUT,
+      url: `${baseUrl}/users/v1/name1/email`,
+      body: {
+        email: 'mail3@mail.com'
+      },
+      headers: {
+        Authorization: 'Bearer <token>'
+      },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});
